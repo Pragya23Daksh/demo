0767431b732a0e8708faf7eb00807af6ed8b2c35
Pragya23Daksh <prgdaksh@gmail.com>
2025-03-26 11:27:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,15 +43,15 @@ function App() {
   return (
     <Router>
       <div>
-        <h1>My Vite React App</h1>
+        <h1>My Vite React App 2</h1>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
+            <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
